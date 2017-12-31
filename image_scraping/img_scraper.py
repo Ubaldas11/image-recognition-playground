d7db3d90af27998ab6c78c0c1ef1772fe9221ecc
Ubaldas11 <jurgis.kargaudas@gmail.com>
2017-12-31 13:54:58 +0200

    Added image scraping from Flickr

---
+++ image_scraping/img_scraper.py
@@ -0,0 +1,51 @@
+from flickrapi import FlickrAPI  # https://pypi.python.org/pypi/flickrapi
+import urllib
+import os
+import config
+from random import randint
+import time
+
+def get_photos(keyword, size='original', max_nb_img=-1):
+    
+    flickr = FlickrAPI(config.API_KEY, config.API_SECRET)
+        
+    if size == 'thumbnail':
+        size_url = 'url_t'
+    elif size == 'square':
+        size_url = 'url_q'
+    elif size == 'medium':
+        size_url = 'url_c'
+    elif size == 'original':
+        size_url = 'url_o'
+    
+    results_folder = config.IMG_FOLDER + keyword.replace(" ", "_") + "/"
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+
+    photos = flickr.walk(
+                    text=keyword,
+                    extras=size_url,
+                    license='1,2,4,5',
+                    per_page=50)
+    
+    urls = []
+    count = 0
+    
+    for photo in photos:
+        t = randint(1, 3)
+        time.sleep(t)
+        count += 1
+        if max_nb_img != -1:
+            if count > max_nb_img:
+                print('Reached maximum number of images to download')
+                break
+        try:
+            url=photo.get(size_url)
+            urls.append(url)
+            
+            urllib.request.urlretrieve(url,  results_folder + str(count) +".jpg")
+            print('Downloading image #' + str(count) + ' from url ' + url)
+        except Exception as e:
+            print(e, 'Download failure')
+                            
+    print("Total images downloaded:", str(count - 1))
